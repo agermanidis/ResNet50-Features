8108e6d7b27368cf5d951b5acd8c0d6709e524d5
Anastasis Germanidis <agermanidis@gmail.com>
2019-08-16 15:19:48 -0400

    ues preprocess function from resnet instead of vgg16

--- runway_model.py
+++ runway_model.py
@@ -1,13 +1,11 @@
-from keras import applications
-from keras.preprocessing import image
-from keras.applications.vgg16 import VGG16
-from keras.applications.vgg16 import preprocess_input
+from keras.applications.resnet50 import ResNet50
+from keras.applications.resnet50 import preprocess_input
 import numpy as np
 import runway
 
 @runway.setup
 def setup():
-  return applications.resnet50.ResNet50(weights='imagenet', include_top=False, pooling='avg')
+  return ResNet50(weights='imagenet', include_top=False, pooling='avg')
 
 @runway.command('extract_features', inputs={'image': runway.image}, outputs={'features': runway.vector(2048)})
 def extract_features(model, inputs):
